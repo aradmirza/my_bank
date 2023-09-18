226d6c0727a90c67ce9f6ff4dea54af93588655d
aradmirza <aradmirza3@gmail.com>
2023-09-18 16:50:15 +0600

    first commit

---
+++ js/login.js
@@ -0,0 +1,28 @@
+
+
+document.getElementById('submit-btn').addEventListener('click', function(){
+  
+  const emailField = document.getElementById('user-email');
+  const email = emailField.value;
+  console.log(email);
+
+
+  const passwordField = document.getElementById('user-password');
+  const password = passwordField.value;
+
+  
+
+  if(email === "arad" && password === "fatema"){
+
+    location.href = "development/bank.html";
+
+  }
+  else{
+    alert("Invalid");
+  }
+
+
+
+
+
+})
