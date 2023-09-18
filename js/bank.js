e02b30e6804b2cefa39a79f8caf2b9e55507c7b8
aradmirza <aradmirza3@gmail.com>
2023-09-19 00:26:43 +0600

    third commit

--- js/bank.js
+++ js/bank.js
@@ -2,12 +2,23 @@
 document.getElementById('deposit-btn').addEventListener('click', function(){
 
   const depositAmmount = document.getElementById('ammount-deposit');
-  const depositOfAmmount = depositAmmount.value;
+  const newDepositAmmount = depositAmmount.value;
 
   
 
   const displayDeposit = document.getElementById('displayDeposit-ammount')
-  const displayDepositMoney = displayDeposit.innerText;
-  displayDeposit.innerText = depositOfAmmount;
+  const previousTotal = displayDeposit.innerText;
 
+  const currentDeposit = parseFloat(newDepositAmmount) + parseFloat(previousTotal);
+
+  const fixed2 = currentDeposit.toFixed(2);
+
+  displayDeposit.innerText = fixed2;
+
+
+
+
+
+
+  depositAmmount.value = "";
 })
