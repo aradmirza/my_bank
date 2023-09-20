9e8d9d4218e3eee8d081e8c665ed803b359aab48
aradmirza <aradmirza3@gmail.com>
2023-09-20 19:49:27 +0600

    add

--- js/login.js
+++ js/login.js
@@ -1,28 +1,34 @@
 
 
-document.getElementById('submit-btn').addEventListener('click', function(){
+// document.getElementById('submit-btn').addEventListener('click', function(){
   
-  const emailField = document.getElementById('user-email');
-  const email = emailField.value;
-  console.log(email);
+//   const emailField = document.getElementById('user-email');
+//   const email = emailField.value;
+//   console.log(email);
 
 
-  const passwordField = document.getElementById('user-password');
-  const password = passwordField.value;
+//   const passwordField = document.getElementById('user-password');
+//   const password = passwordField.value;
 
   
 
-  if(email === "arad2" && password === "gogo"){
+//   // if(email === "arad2" && password === "gogo"){
 
-    location.href = "development/bank.html";
+//   //   location.href = "development/bank.html";
 
-  }
-  else{
-    alert("Invalid");
-  }
-
-
+//   // }
+//   // else{
+//   //   alert("Invalid");
+//   // }
 
 
 
-})
+
+
+// })
+
+
+function submit(){
+  document.getElementById("main1").style.display ="none"
+  document.getElementById("main2").style.display ="block"
+}
